abf3014d1793493fa49c2c0b44dedc29d1fc69ab
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2024-10-25 14:49:21 -0400

    Update cato-conn.sh

--- cato-conn.sh
+++ cato-conn.sh
@@ -2,4 +2,4 @@
 curl https://raw.githubusercontent.com/jrabinovitz-ideal/Graylog/refs/heads/main/filebeat.yml -o /tmp/filebeat.yml
 mv /tmp/filebeat.yml /etc/filebeat/
 
-(crontab -l 2>/dev/null || true; echo "5 * * * * python3 /root/eventsFeed.py -I 4536 -K 25E6E509811F616D75819B2442406275 -t Connectivity -p -r 1 2>&1 >> /var/log/cato_conn.json") | crontab -
+(crontab -l 2>/dev/null || true; echo "7 * * * * python3 /root/eventsFeed.py -I 4536 -K 25E6E509811F616D75819B2442406275 -t Connectivity -p -r 1 2>&1 >> /var/log/cato_conn.json") | crontab -
