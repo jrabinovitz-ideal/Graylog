858522231ce0dbda2c31314723f49a448433c3bf
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2025-09-16 10:34:35 -0400

    Create beats_stage1.ps1

---
+++ beats_stage1.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+Invoke-WebRequest -Uri 'https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.16.2-windows-x86_64.zip' -Outfile "C:\Windows\Temp\winlogbeat.zip"
+
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/jrabinovitz-ideal/Graylog/refs/heads/main/winlogbeat.yml' -Outfile "C:\Windows\Temp\"
+
+Expand-Archive -LiteralPath 'c:\windows\temp\winlogbeat.zip' -DestinationPath C:\Windows\Temp\
+
+#Invoke-Command -ScriptBlock {c:\windows\temp\CrowdStrike_Installer\FalconSensor_Windows.exe /install /quiet /norestart CID=$CS_CID}
